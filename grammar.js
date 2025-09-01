3c726d5ba0ef5c70f197efb92329f4317d4ec15b
Bss <mx.bss@proton.me>
2025-09-01 16:18:49 +0200

    define `nil` and `inf` as builtin constants
    `nil` and `inf` are not context relative, so it feels more natural to
    define them as builtin constants.
    
    fix: broken tests

--- grammar.js
+++ grammar.js
@@ -294,8 +294,11 @@ module.exports = grammar({
 			$.char,
 			$.string,
 			$.bool,
+            $.builtin_constant
 			// $.pi_statement
 		),
+
+        builtin_constant: $ => choice("nil", "inf"),
 
 		// pi_statement: $ => seq(optional($.number), "pi"),
 		number: $ => choice(
@@ -356,8 +359,6 @@ module.exports = grammar({
 			$.instance_var
 		),
 		builtin_var: $ => field("name", choice(
-			"inf",
-			"nil",
 			"thisFunction",
 			"thisFunctionDef",
 			"thisMethod",
